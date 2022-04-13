e3ff3370788e346e5c1304de78b8507b042a64db
adityandinesh <adityandinesh001@gmail.com>
2022-04-13 21:33:13 +0530

    updated animations

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,24 +1,65 @@
+import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import LogoTitle from '../../assets/images/logo-s.png'
 import './index.scss'
+import AnimatedLetters from '../AnimatedLetters'
 
-const Home =() =>{
-    return(
-        <div className="container home-page">
-            <div className="text-zone">
-                <h1>
-                    Hi <br /> I'am
-                    <img src={LogoTitle} alt="developer" />
-                dityan  
-                <br />
-                Web developer
-                </h1>
-               
-                <h2>Frontend Developer / Javascrpt Pro</h2>
-                <Link to="/contact" className='flat-button'>CONTACT ME</Link>
-            </div>
-        </div>
-    )
+const Home = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const nameAray = ['d', 'i', 't', 'y', 'a', 'n']
+  const jobArray = [
+    'w',
+    'e',
+    'b',
+    '',
+    'd',
+    'e',
+    'v',
+    'e',
+    'l',
+    'o',
+    'p',
+    'e',
+    'r',
+    '.',
+  ]
+
+  useEffect(() => {
+     setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 4000)
+  },[])
+
+  return (
+    <div className="container home-page">
+      <div className="text-zone">
+        <h1>
+          <span className={letterClass}>H</span>
+          <span className={`${letterClass} _12`}>i,</span>
+          <br />
+          <span className={`${letterClass} _13`}>I</span>
+          <span className={`${letterClass} _14`}>'m</span>
+          <img src={LogoTitle} alt="developer" />
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={nameAray}
+            idx={15}
+          />
+          <br />
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={jobArray}
+            idx={22}
+          />
+        </h1>
+
+        <h2>Frontend Developer / ReactJS</h2>
+        <Link to="/contact" className="flat-button">
+          CONTACT ME
+        </Link>
+      </div>
+    </div>
+  )
 }
 
 export default Home
