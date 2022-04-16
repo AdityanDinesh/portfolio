389b5337053b417cb7e3d1c6054869b011100307
adityandinesh <adityandinesh001@gmail.com>
2022-04-16 23:29:08 +0530

    Updated about page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -42,7 +42,7 @@ const About = () => {
           </p>
           <p>
             If I need to define myself in one sentence that would be a family
-            person, father of a beautiful daughter, a sports fanatic,
+            person,a sports fanatic,
             photography enthusiast, and tech-obsessed!!!
           </p>
         </div>
